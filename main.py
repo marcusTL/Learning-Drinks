21765f5076d7c7124f4d19217762afa0fa813c4a
marcusTL <marcus.gaming.laursen@gmail.com>
2017-11-13 11:04:51 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+#display setup
+
+display_width = 600
+display_height = 500
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('Open the dungeon') 
+clock = pygame.time.Clock()
+
+#colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+green = (0, 255, 0)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+
+#game loop
+crashed = False
+
+while not crashed:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+
+        print(event)
+        
+    gameDisplay.fill(black)
+    pygame.display.update()
+    clock.tick(60)
+    
+pygame.quit()
+quit()
